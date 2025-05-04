9ce538e4bc03f8227ed9b2cbad86a46ef2ba0cd0
Christopher Pezza <chris.pezza@hey.com>
2025-05-04 16:36:21 -0400

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeModeScript } from "flowbite-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -33,6 +34,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-gray-50 antialiased dark:bg-gray-800`}
 			>
 				<LayoutWrapper>{children}</LayoutWrapper>
+				<SpeedInsights/>
 			</body>
 		</html>
 	);
