c31d06cfbb9962c965bf6358f935d6410ab64ef3
Christopher Pezza <chris.pezza@hey.com>
2025-10-29 10:20:32 -0400

    Update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeModeScript } from "flowbite-react";
 import type { Metadata } from "next";
@@ -34,7 +35,8 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-gray-50 antialiased dark:bg-gray-800`}
 			>
 				<LayoutWrapper>{children}</LayoutWrapper>
-				<SpeedInsights/>
+				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	);
